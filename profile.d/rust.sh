43c92cab54cb8753367adbe209bddf046862e1a0
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-05-28 03:03:41 -0700

    Fixed gitignore, synchronizing config

--- profile.d/rust.sh
+++ profile.d/rust.sh
@@ -2,15 +2,13 @@
 
 if test -d "${CARGO_HOME-}"
 then
-  export PATH="${CARGO_HOME}/bin${PATH:+:${PATH}}"
+  export PATH="${PATH:+${PATH}:}${CARGO_HOME}/bin"
 elif test -d "${HOME}/.local/share/cargo"
 then
-  export PATH="${HOME}/.local/share/cargo/bin${PATH:+:${PATH}}"
+  export PATH="${PATH:+${PATH}:}${HOME}/.local/share/cargo/bin"
 elif test -d "${HOME}/.cargo"
 then
-  export PATH="${HOME}/.cargo/bin${PATH:+:${PATH}}"
+  export PATH="${PATH:+${PATH}:}${HOME}/.cargo/bin"
 fi
-
-# vim:ft=sh
 
 # vim:ft=sh
