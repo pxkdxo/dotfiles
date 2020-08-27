77b6c3b740c78fc952254fb314e6ee5d2800b840
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-08-26 19:43:09 -0700

    Syncing profile environment mods

--- profile.d/cabal.sh
+++ profile.d/cabal.sh
@@ -1,11 +1,30 @@
 # cabal.sh: cabal path config
 
-if test -d "${CABAL_HOME}"; then
-  export PATH="${PATH:+${PATH}:}${CABAL_HOME}/bin"
+if test -n "${CABAL_HOME}"; then
+  case ":${PATH}:" in
+    *:"${CABAL_HOME}/bin":*) ;;
+    *) export PATH="${PATH:+${PATH}:}${CABAL_HOME}/bin" ;;
+  esac
 elif test -d "${HOME}/.local/share/cabal"; then
-  export PATH="${PATH:+${PATH}:}${HOME}/.local/share/cabal/bin"
+  case ":${PATH}:" in
+    *:"${HOME}/.local/share/cabal/bin":*) ;;
+    *) export PATH="${PATH:+${PATH}:}${HOME}/.local/share/cabal/bin" ;;
+  esac
+elif test -d "${HOME}/.local/cabal"; then
+  case ":${PATH}:" in
+    *:"${HOME}/.local/cabal/bin":*) ;;
+    *) export PATH="${PATH:+${PATH}:}${HOME}/.local/cabal/bin" ;;
+  esac
 elif test -d "${HOME}/.cabal"; then
-  export PATH="${PATH:+${PATH}:}${HOME}/.cabal/bin"
+  case ":${PATH}:" in
+    *:"${HOME}/.cabal/bin":*) ;;
+    *) export PATH="${PATH:+${PATH}:}${HOME}/.cabal/bin" ;;
+  esac
+elif test -d "${HOME}/cabal"; then
+  case ":${PATH}:" in
+    *:"${HOME}/cabal/bin":*) ;;
+    *) export PATH="${PATH:+${PATH}:}${HOME}/cabal/bin" ;;
+  esac
 fi
 
 # vim:ft=sh
