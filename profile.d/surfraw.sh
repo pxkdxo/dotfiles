85b19e6ea9d9c3ced2998ec39577ce843e4faca4
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-01-17 18:00:38 -0800

    Fixed adding of duplicate path elements

--- profile.d/surfraw.sh
+++ profile.d/surfraw.sh
@@ -1,14 +1,21 @@
-# surfraw.sh: surfraw elvi paths
+# surfraw.sh: surfraw elvi path config
 
 if command -v surfraw > /dev/null; then
   if test -d /usr/local/lib/surfraw; then
-    export PATH="${PATH:+${PATH}:}/usr/local/lib/surfraw"
-  fi
-  if test -d /usr/lib/surfraw; then
-    export PATH="${PATH:+${PATH}:}/usr/lib/surfraw"
-  fi
-  if test -d /lib/surfraw; then
-    export PATH="${PATH:+${PATH}:}/lib/surfraw"
+    case  ":${PATH}:" in
+      *:"/usr/local/lib/surfraw":*) ;;
+      *) export PATH="${PATH:+${PATH}:}/usr/local/lib/surfraw" ;;
+    esac
+  elif test -d /usr/lib/surfraw; then
+    case ":${PATH}:" in
+      *:"/usr/lib/surfraw":*) ;;
+      *) export PATH="${PATH:+${PATH}:}/usr/lib/surfraw" ;;
+    esac
+  elif test -d /lib/surfraw; then
+    case ":${PATH}:" in
+      *:"/lib/surfraw":*) ;;
+      *) export PATH="${PATH:+${PATH}:}/lib/surfraw" ;;
+    esac
   fi
 fi
 
