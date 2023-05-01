6728043c7b3c2bb92f085346f7df46aa9de250a6
Patrick DeYoreo <pdeyoreo@gmail.com>
2023-05-01 11:30:57 -0700

    Pulling latest changes

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/pat/.local/share/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/pat/.local/share/fzf/bin"
+if [[ ! "$PATH" == */Users/pat/.local/share/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/Users/pat/.local/share/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-# [[ $- == *i* ]] && source "/home/pat/.local/share/fzf/shell/completion.bash" 2> /dev/null
+# [[ $- == *i* ]] && source "/Users/pat/.local/share/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-# source "/home/pat/.local/share/fzf/shell/key-bindings.bash"
+source "/Users/pat/.local/share/fzf/shell/key-bindings.bash"
--- profile.d/gpg-agent.sh
+++ profile.d/gpg-agent.sh
@@ -2,22 +2,25 @@
 # gpg-agent.sh: gpg-agent config
 # see gpg-agent(1)
 
-AGENT_SOCK="$(gpgconf --list-dirs agent-socket)"
+if command -v gpgconf > /dev/null
+then
+  AGENT_SOCK="$(gpgconf --list-dirs agent-socket)"
 
-# '--use-standard-socket' should work from version 2 onwards
-if ! test -S "${AGENT_SOCK}"; then
-  gpg-agent --daemon --use-standard-socket 1> /dev/null 2>&1
-fi
+  # '--use-standard-socket' should work from version 2 onwards
+  if ! test -S "${AGENT_SOCK}"; then
+    gpg-agent --daemon --use-standard-socket 1> /dev/null 2>&1
+  fi
 
-# Export the current terminal
-GPG_TTY="${TTY:-$(tty)}"
-export GPG_TTY
+  # Export the current terminal
+  GPG_TTY="${TTY:-$(tty)}"
+  export GPG_TTY
 
-# Configure SSH to use gpg-agent
-unset -v SSH_AGENT_PID
-if test "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne "$$"; then
-  SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-  export SSH_AUTH_SOCK
+  # Configure SSH to use gpg-agent
+  unset -v SSH_AGENT_PID
+  if test "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne "$$"; then
+    SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+    export SSH_AUTH_SOCK
+  fi
 fi
 
 # vi:ft=sh
