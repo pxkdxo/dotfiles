b19d01655ae4391f1508a0d47186124c4db24514
Patrick DeYoreo <pdeyoreo@gmail.com>
2025-03-04 11:47:28 -0800

    Added profile.d/java.sh to set JAVA_HOME

---
+++ profile.d/java.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+# java.sh: java environment config
+
+if test -d /lib/jvm/default
+then
+  JAVA_HOME="/lib/jvm/default"
+else
+  for directory in /lib/jvm/java-*
+  do
+    if test -d "${directory}"
+    then
+      JAVA_HOME="${directory}"
+    fi
+  done
+fi
+if test -n "${JAVA_HOME}"
+then
+  export JAVA_HOME
+fi
+
+# vim:ft=sh
