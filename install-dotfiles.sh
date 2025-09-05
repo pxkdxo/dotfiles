dbe708b4de794e7ce5c10a078775393b3caa4361
Patrick DeYoreo <patrick.deyoreo@fox.com>
2025-09-04 22:27:40 -0700

    Updated install script

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
-#
-# Create a symlink in the home directory for each top-level file in this repository, prefixing the filename of each link with a dot.
+# install-dotfiles.sh
+# Create symbolic links in the invoking user's home directory for each
+# top-level file and directory tracked by this repository. The name of
+# the link will match the name of the target, prefixed by a period ('.').
 
 set -o errexit
 
@@ -8,28 +10,90 @@ argzero_basename="${0##*/}"
 argzero_dirname="${0%"${argzero_basename}"}"
 argzero_dirname="${argzero_dirname:-.}"
 
+usage="${argzero_basename} [-nifb] [-S suffix]"
+
+print_help() {
+  cat << EOF
+usage: ${usage}
+
+${argzero_basename} - link dotfiles into a user's home directory
+
+Create symbolic links in the invoking user's home directory for each
+top-level file and directory tracked by this repository. The name of
+the link will match the name of the target, prefixed by a period ('.').
+EOF
+}
+
+opt_backup=''
+opt_suffix=''
+if test -t 0; then
+  opt_overwrite='--interactive'
+else
+  opt_overwrite=''
+fi
+
+OPTIND=1
+option=''
+optstr=':hnifbS:'
+
+while getopts "${optstr}" option; do
+  case "${option}" in
+    'h')
+      print_help
+      exit 2
+      ;;
+    'n') opt_overwrite='' ;;
+    'i') opt_overwrite='--interactive' ;;
+    'f') opt_overwrite='--force' ;;
+    'b') opt_backup='--backup' ;;
+    'S') opt_suffix="--suffix=${OPTARG}" ;;
+    '?')
+      printf '%s: -%c: unrecognized option\n' "${argzero_basename}" "${OPTARG}" >&2
+      printf 'usage: %s' "${usage}" >&2
+      exit 2
+      ;;
+    ':')
+      printf '%s: -%c: missing required argument\n' "${argzero_basename}" "${OPTARG}" >&2
+      printf 'usage: %s' "${usage}" >&2
+      exit 2
+      ;;
+  esac
+done
+shift "$((OPTIND - 1))"
+
+if test "$#" -ne 0; then
+  printf '%s: received too many arguments\n' "${argzero_basename}" >&2
+  printf 'usage: %s' "${usage}" >&2
+  exit 2
+fi
+
+set --
+if test -n "${opt_backup}"; then
+  set -- "$@" "${opt_backup}"
+fi
+if test -n "${opt_suffix}"; then
+  set -- "$@" "${opt_suffix}"
+fi
+if test -n "${opt_overwrite}"; then
+  set -- "$@" "${opt_overwrite}"
+fi
+
 cd -- "${argzero_dirname}"
 
-tree_abspath="$(pwd -P)"
-home_abspath="$(cd && pwd -P)"
+tree_path="$(pwd -P && printf -- '%c' '@')"
+tree_path="${tree_path%?@}"
+home_path="$(cd && pwd -P && printf -- '%c' '@')"
+home_path="${home_path%?@}"
 
-case "${tree_abspath}" in
-  "${home_abspath}")
-    tree_relative_to_home_path="."
-    ;;
-  "${home_abspath}"/*)
-    tree_relative_to_home_path="${tree_abspath#"${home_abspath}/"}"
-    ;;
-  *)
-    tree_relative_to_home_path="${home_abspath#"${tree_abspath}/"}"
-    ;;
-esac
-
+# shellcheck disable=SC2016
 git ls-tree --name-only -z HEAD | xargs -0 -n 1 -I '{}' -o -- sh -c '
-case "$3" in (.*|*.md) exit 0 ;;
+case "$1" in (.*|*.md) exit 0 ;;
 esac
+target="$1/$3"
+link="$2/.$3"
+shift 3
 set -x
-ln -snfbv -- "$1/$3" "$2/.$3"
-' -- "${tree_relative_to_home_path}" "${home_abspath}" '{}' || :
+ln "$@" --verbose --symbolic --relative --no-dereference -T -- "${target}" "${link}"
+' -- "${tree_path}" "${home_path}" '{}' "$@" 2>&1 || :
 
 exit 0
