eff5939c7ede1589f0157d51241b33589b5ecc9d
Patrick DeYoreo <patrick.deyoreo@fox.com>
2025-09-05 21:49:03 -0700

    Added repo installation script

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -3,6 +3,8 @@
 # Create symbolic links in the invoking user's home directory for each
 # top-level file and directory tracked by this repository. The name of
 # the link will match the name of the target, prefixed by a period ('.').
+# Alternatively, if the name of a directory is given as an argument, then
+# it will be used instead of the invoking user's home directory.
 
 set -o errexit
 
@@ -10,7 +12,7 @@ argzero_basename="${0##*/}"
 argzero_dirname="${0%"${argzero_basename}"}"
 argzero_dirname="${argzero_dirname:-.}"
 
-usage="${argzero_basename} [-nifb] [-S suffix]"
+usage="${argzero_basename} [-nifb] [-S suffix] [--] [TARGET_DIRECTORY]"
 
 print_help() {
   cat << EOF
@@ -21,6 +23,8 @@ ${argzero_basename} - link dotfiles into a user's home directory
 Create symbolic links in the invoking user's home directory for each
 top-level file and directory tracked by this repository. The name of
 the link will match the name of the target, prefixed by a period ('.').
+Alternatively, if the name of a directory is given as an argument, then
+it will be used instead of the invoking user's home directory.
 EOF
 }
 
@@ -61,10 +65,24 @@ while getopts "${optstr}" option; do
 done
 shift "$((OPTIND - 1))"
 
-if test "$#" -ne 0; then
+if test "$#" -gt 1; then
   printf '%s: received too many arguments\n' "${argzero_basename}" >&2
   printf 'usage: %s' "${usage}" >&2
   exit 2
+fi
+
+cd -- "${argzero_dirname}"
+
+tree_path="$(pwd -P && printf -- '%c' '@')"
+tree_path="${tree_path%?@}"
+
+if test "$#" -eq 1; then
+  home_path="$(cd -- "$1" && pwd -P && printf -- '%c' '@')"
+  home_path="${home_path%?@}"
+  shift
+else
+  home_path="$(cd && pwd -P && printf -- '%c' '@')"
+  home_path="${home_path%?@}"
 fi
 
 set --
@@ -77,13 +95,6 @@ fi
 if test -n "${opt_overwrite}"; then
   set -- "$@" "${opt_overwrite}"
 fi
-
-cd -- "${argzero_dirname}"
-
-tree_path="$(pwd -P && printf -- '%c' '@')"
-tree_path="${tree_path%?@}"
-home_path="$(cd && pwd -P && printf -- '%c' '@')"
-home_path="${home_path%?@}"
 
 # shellcheck disable=SC2016
 git ls-tree --name-only -z HEAD | xargs -0 -n 1 -I '{}' -o -- sh -c '
