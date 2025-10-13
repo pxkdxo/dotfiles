deca6bfed84dad506a8a4e9fd8f1990488a71615
Patrick DeYoreo <patrick.deyoreo@fox.com>
2025-10-12 17:45:46 -0700

    Updated gpg-agent.sh

--- profile.d/gpg-agent.sh
+++ profile.d/gpg-agent.sh
@@ -2,22 +2,26 @@
 # gpg-agent.sh: gpg-agent config
 # see gpg-agent(1)
 
+# Fix for passphrase prompt on the correct tty
+if command -v gpg-connect-agent > /dev/null
+then
+  gpg-connect-agent updatestartuptty /bye > /dev/null 2>&1
+fi
+
+  # Configure SSH to use gpg-agent (if supported)
 if command -v gpgconf > /dev/null
 then
-  # '--use-standard-socket' should work from version 2 onwards
-  if ! AGENT_SOCK="$(gpgconf --list-dirs agent-socket)"  2> /dev/null || ! test -S "${AGENT_SOCK}"; then
-    gpg-agent --daemon --use-standard-socket 1> /dev/null 2>&1
-  fi
-
-  # Export the current terminal
-  export GPG_TTY="${TTY:-$(tty)}"
-
-  # Configure SSH to use gpg-agent
-  unset SSH_AGENT_PID
-  if test "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne "$$"; then
-    SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-    export SSH_AUTH_SOCK
-  fi
+  case ":$(gpgconf --list-options gpg-agent 2> /dev/null | tr -s '\n' ':'):" in
+    *:enable-ssh-support:*)
+      unset SSH_AGENT_PID
+      if test "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne "$$" && SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+      then
+        export SSH_AUTH_SOCK
+      else
+        unset SSH_AUTH_SOCK
+      fi
+      ;;
+  esac
 fi
 
 # vi:ft=sh
