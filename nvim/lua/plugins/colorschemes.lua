e422d87a7c6ab2cbcb79f616755ebec0aef8829b
Patrick DeYoreo <pdeyoreo@gmail.com>
2026-01-24 21:38:52 -0800

    New files from macos

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -31,7 +31,7 @@ return {
     priority = 1000,
     opts = {
       -- Set light or dark variant
-      variant = "light", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+      variant = "auto", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
       -- Enable transparent background
       transparent = true,
       -- Reduce the overall saturation of colours for a more muted look
